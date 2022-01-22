911982e336b53f4fee07d37dbb1b63a46c383ba7
Abe Jay <officialjabe@gmail.com>
2022-01-22 15:01:27 -0500

    created draw objects to populate canvas

--- main.js
+++ main.js
@@ -1,2 +1,55 @@
+// RAM = Random Access Memory = variables
+// CPU = Central Proccesing Unit = logic execution
+// GPU = Graphics Proccsing Unit = logic for images and relates
+
+// FPS = Frames Per Second = how fast our game runs
+
 const debug = document.querySelector('.debug');
 debug.innerText = 'hello world';
+
+const canvas = document.querySelector('canvas');
+// get the functions for 2d rendering on our canvas
+const context = canvas.getContext('2d');
+
+
+
+let x = 50;
+
+const FPS = 60;
+
+class DrawObject{
+    x = 50;
+    y = 50;
+    width = 50;
+    height = 100;
+    color = 'red';
+    draw() {
+        //set the draw color of my canvas to my color
+        context.fillStyle = this.color;
+
+         // draw a rectangle
+        context.fillRect(this.x, this.y, this.width, this.height);
+
+        this.x += 3;
+    }
+}
+
+class Mario extends DrawObject{
+
+}
+
+const mario = new Mario();
+const mario2 = new Mario();
+mario2.y = 200;
+mario2.x = 13;
+mario2.color = 'yellow';
+
+function draw(){
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    console.log('mario was drawn');
+    mario.draw();
+    mario2.draw();
+    setTimeout(draw, 1000 / FPS);
+}
+
+draw();
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Mario Gameproject Remake
+
+## I want to make a remake of a platformer using javascript and canvas technology
+
+[] have a class for any of the objects that i make in the game
+    [x] DrawObject
+        [x] draw
+        [] move
+    [x] Mario inherits DrawObject
+
+    [] Enemies inherits DrawObject
+
+    [] Obstacles inherits DrawObject
+
+[X] create a system that draws my character every frame
+
+[] give my charater the ability to move in any direction
+
+[] apply gravity to my charcter
